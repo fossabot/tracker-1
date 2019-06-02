29fb04ff130d9528b38930e591f74ffe6c5feb70
Mya Pitzeruse <j.mya.pitz@gmail.com>
2019-06-01 20:21:36 -0500

    gh-14: added swagger output and made DOWNSTREAM the default direction

--- api/api.go
+++ api/api.go
@@ -1,3 +1,3 @@
 package api
 
-//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --gogo_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. dts.proto
+//go:generate protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --gogo_out=plugins=grpc:. --grpc-gateway_out=logtostderr=true:. --swagger_out=logtostderr=true:. dts.proto
